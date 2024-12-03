5ad23f5653e74e60f829f66e59bd8f6871d4e687
Akbar <iron.man96321@gmail.com>
2024-12-03 10:37:06 +0600

    first commit

---
+++ my-aws-project/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "awsproject725fb6f9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
